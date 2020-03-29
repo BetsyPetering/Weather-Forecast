678c5f2bba1566a85aedc885ebe5a6f2e9c74244
PinkAttitude77 <betsy.petering@sbcglobal.net>
2020-03-28 23:29:26 -0700

    fimal

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,7 +3,7 @@ var findCity = document.getElementById("citySearch");
 var tempF;
 var cityList = JSON.parse(window.localStorage.getItem("storedCities")) || [];
 
-currentDate = moment().format("l"); // 3/21/2020
+currentDate = moment().format("l");
 
 start();
 
@@ -11,7 +11,7 @@ function start() {
   if (cityList.length != 0) {
     for (i = 0; i < cityList.length; i++) {
       var cityButton = $("<button>").attr("value", cityList[i]);
-      cityButton.attr("class", "btn");
+      cityButton.attr("id", "one");
       cityButton.attr("onclick", "currentInfo('" + cityList[i] + "')");
       cityButton.text(cityList[i]);
 
@@ -29,7 +29,7 @@ function getCityName() {
 
   if (cityList.indexOf(city) == -1) {
     var cityButton = $("<button>").attr("value", city);
-    cityButton.attr("class", "btn");
+    cityButton.attr("id", "one");
     cityButton.attr("onclick", "currentInfo('" + city + "')");
     cityButton.text(city);
 
@@ -40,7 +40,8 @@ function getCityName() {
   }
 }
 
-function currentInfo(city) {  //have to get  current data in separate API than the 5 day forecast
+function currentInfo(city) {
+  //have to get  current data in separate API than the 5 day forecast
 
   var queryURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -51,7 +52,6 @@ function currentInfo(city) {  //have to get  current data in separate API than t
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-
     currentTemp = Math.round((response.main.temp - 273.15) * 1.8 + 32);
     windSpeed = Math.round(response.wind.speed);
 
@@ -85,31 +85,25 @@ function currentUv(latitude, longitude) {
     latitude +
     "&lon=" +
     longitude;
- 
+
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-
     $(".uv").text("UV Index: " + response.value);
     // $(".uva").text(response.value);
     if (response.value < 3) {
-      $(".uva").addClass("favorable");
+      $(".uv").addClass("favorable");
     } else if (response.value < 8) {
-      $(".uva").addClass("moderate");
+      $(".uv").addClass("moderate");
     } else {
-      $(".uva").addClass("severe");
+      $(".uv").addClass("severe");
     }
   });
 }
 
-// function clearBox(city) {
-//     document.getElementById(fiver).innerHTML = "";
-//     fiveDay(city);
-// }
-
 function fiveDay(city) {
-  $(".fiveDay").empty();
+  $(".three").empty();
   var queryURL =
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
     city +
@@ -118,11 +112,10 @@ function fiveDay(city) {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-
     for (i = 0; i < response.list.length; i++) {
       if (response.list[i].dt_txt.indexOf("15") != -1) {
         var divTag = $("<div>").attr("class", "dayBlock");
-        $(".fiveDay").append(divTag);
+        $(".three").append(divTag);
 
         var newTag = $("<img>").attr(
           "src",
@@ -140,7 +133,6 @@ function fiveDay(city) {
         // Converts the temp to Kelvin with the below formula
         tempF = Math.round((response.list[i].main.temp - 273.15) * 1.8 + 32);
         // (0K − 273.15) × 9/5 + 32 = -459.7°F
-
 
         var tempTag = $("<p>").attr("class", "temp");
         tempTag.text("Temperature " + tempF + "F");
